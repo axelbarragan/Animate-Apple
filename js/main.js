0b963b76c03382391d4d21479340dae51df3628e
Axel Barragán <axelmontes92@gmail.com>
2019-09-19 16:54:26 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,12 +33,24 @@ $(document).ready(function() {
   var tlSecondScroll = new TimelineMax();
 
   tlSecondScroll
-    .to('.iphone1', 3, { x: "-50%" })
-    .to('.iphone2', 3, { x: "50%"  }, "-=3")
+    .to('.iphone1', 3, { x: "-54%" })
+    .to('.iphone2', 3, { x: "54%"  }, "-=3")
     .from('.iphone1-text', 0.3, { autoAlpha: 0 }, "-=3")
     .from('.iphone2-text', 0.3, { autoAlpha: 0 }, "-=3")
     .to('.iphone1-text', 3, { x: "-30%" }, "-=3")
     .to('.iphone2-text', 3, { x: "30%"  }, "-=3")
+
+    .set('.iphone-stick', {display: "block"})
+
+    .to('.iphone1-img-behind', 3, {x: "-54%"})
+    .to('.iphone2-img-behind', 3, {x: "54%"}, "-=3")
+
+    .to('.iphone1-img', 0.5, {autoAlpha: 0}, "-=3")
+    .to('.iphone2-img', 0.5, {autoAlpha: 0}, "-=3")
+
+    .to('.iphone1-text', 0.3, {autoAlpha: 0}, "-=3")
+    .to('.iphone2-text', 0.3, {autoAlpha: 0, ease:Power1.easeNone}, "-=3")
+    //.to('.iphone2-text', 0.3, {autoAlpha: 0}, "-=3")
 
    var scene2 = new ScrollMagic.Scene({
      triggerElement: '.trigger2',
